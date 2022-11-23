a507f027ee679411058d73a44ccca777bf415762
Bastientt <98900238+Bastientt@users.noreply.github.com>
2022-11-23 18:27:24 +0100

    feat:add structures for "avion" and "piste"

--- moteur.h
+++ moteur.h
@@ -3,4 +3,39 @@
 
 
 
+typedef enum TypeAvion{
+    AVIONLIGNE,
+    AVIONAFFAIRE,
+    AVIONLEGER,
+    UNDEFINED
+}TypeAvion;
+
+typedef enum TypePiste {
+    PETITE,
+    MOYENNE,
+    GRANDE
+}TypePiste;
+
+ 
+typedef struct avion {
+    char identifiant[20];
+    TypeAvion avionType;
+    int etat;
+    int nbPassagers;
+    struct avion* prec;
+    struct avion* suiv;
+}avion;
+
+typedef struct piste{
+    int numPiste;
+    int longueur;
+    TypePiste pisteType;
+    int nbAvionsMax;
+    avion* listeAvion;
+}piste;
+
+avion* initAvion();
+avion* creerAvion(char* id,TypeAvion avionType,int etat,int nbPassagers);
+
+
 #endif
