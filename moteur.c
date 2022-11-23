6a710a8a467174851405fac547b944c34dec9bce
Bastientt <98900238+Bastientt@users.noreply.github.com>
2022-11-23 18:27:46 +0100

    feat: add the basic command for using the struct "avion"

--- moteur.c
+++ moteur.c
@@ -1,1 +1,26 @@
 #include "moteur.h"
+#include <string.h>
+
+avion* initAvion(){
+    avion* elmAvion=malloc(sizeof(avion));
+    elmAvion->avionType=UNDEFINED;
+    elmAvion->etat=-1;
+    strcpy(elmAvion->identifiant,"XXX-XXX-XXX");
+    elmAvion->nbPassagers=-1;
+    elmAvion->prec=NULL;
+    elmAvion->suiv=NULL;
+    return elmAvion;
+}
+
+avion* creerAvion(char* id,TypeAvion avionType,int etat,int nbPassagers){
+    avion* elmAvion=malloc(sizeof(avion));
+    strcpy(elmAvion->identifiant,id);
+    elmAvion->avionType=avionType;
+    elmAvion->etat=etat;
+    elmAvion->nbPassagers=nbPassagers;
+    elmAvion->prec=NULL;
+    elmAvion->suiv=NULL;
+    return elmAvion;
+}
+
+
