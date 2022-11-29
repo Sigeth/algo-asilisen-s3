d60685d41442548289283633ba9d5d1706724c1a
Sigeth <52868615+Sigeth@users.noreply.github.com>
2022-11-29 13:31:13 +0100

    refactor: new init bdd

--- main.c
+++ main.c
@@ -1,34 +1,17 @@
 #include <stdio.h>
 #include "moteur.h"
+#include "sauvegarde.h"
 
 
 int main() {
   //test moteur
     /*
-     *Création de 5 avions pour le test ! 
+     *Création de la base de données pour le test !
      */
-    avion* elmAvion1=initAvion();
-    avion* elmAvion2=initAvion();
-    avion* elmAvion3=initAvion();
-    avion* elmAvion4=initAvion();
-    avion* elmAvion5=initAvion();
-
-    elmAvion1=creerAvion("azerty-uiop",AVIONLEGER,0,15);
-    elmAvion2=creerAvion("Bouffon_AirLine",AVIONLIGNE,1,300);
-    elmAvion3=creerAvion("L'ASILE",AVIONAFFAIRE,0,10);
-    elmAvion4=creerAvion("OMG",AVIONLEGER,0,30);
-    elmAvion5=creerAvion("CONSOLE.LOG(CANNARD!)",AVIONLIGNE,0,200);
-
-    elmAvion1=enfile(elmAvion1,elmAvion2);
-    elmAvion1=enfile(elmAvion1,elmAvion3);
-    elmAvion1=enfile(elmAvion1,elmAvion4);
-    elmAvion1=enfile(elmAvion1,elmAvion5);
-
-    /*
-     * Commande d'affichage de test 
-     */
-    afficheListe(elmAvion1);
-    printf("\nelm crééééééé : %s\n",elmAvion1->identifiant);
+    avion* liste;
+    liste = genBddRandom(liste);
+    
+    afficheListe(liste);
 //fin test moteur
     return 0;
 }
