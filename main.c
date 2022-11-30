2b78d88793699eced56adc0202ac4b3930faff06
Bastientt <98900238+Bastientt@users.noreply.github.com>
2022-11-30 16:31:49 +0100

    refactor : change the system of loading for the struct "avion"

--- main.c
+++ main.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include "moteur.h"
 #include "sauvegarde.h"
-
+#include <stdlib.h>
 
 int main() {
   //test moteur
     /*
      *Création de la base de données pour le test !
      */
-    avion* liste;
-    liste = genBddRandom(liste);
+    avion* liste = malloc(sizeof(avion));
+    liste = loadAvions(liste,"bdd");
     
     afficheListe(liste);
 //fin test moteur
