13e78f0029cfa029bd28880c7f11c6ec5bc0a1d7
Isaure_SB <iservet@orange.fr>
2022-12-05 12:05:01 -0500

    fix: changed the menu with the real list of pistes

--- menu.c
+++ menu.c
@@ -4,25 +4,30 @@
 #include "couleursAffichage.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 
 /*function menu, print the principal menu in the terminal
  *@param avion* liste, the list of the planes
- *@param piste* pisteTEST, the liste of the runways
+ *@param piste* pistes, the liste of the runways
  */
-void menu(listeAvion* liste, piste* PisteTEST){
+void menu(listeAvion* liste, piste* pistes){
 
 	int menuPrincipal = 1;
 	int choixMenu;
 	char* choixAvion = malloc(sizeof(char)*20);
 	int choixPistes;
 	int quitter = 0;
+	char* decollage = malloc(sizeof(char)*3);
+	char* atterissage = malloc(sizeof(char)*3);
+	int pisteAtterissage;
 
 	while(menuPrincipal == 1){
 
 		quitter = 0;
 		//Marche bien pour $COLUMNS = 135
 		couleur("1;35");
+		clrscr();
 		printf("=========================================================== MENU PRINCIPAL ===========================================================\n");
 		printf("|                                                         1 : Afficher avions                                                        |\n");
 		printf("|                                                         2 : Afficher pistes                                                        |\n");
@@ -53,7 +58,34 @@ void menu(listeAvion* liste, piste* PisteTEST){
 					}
 					else{
 						afficheAvion(avionAAfficher);
+						//Si avion au sol
+						if(avionAAfficher->etat == 0){
+							printf("Voulez vous faire décoller l'avion ? (oui ou non)\n");
+							scanf("%s", decollage);
+							if(strcmp(decollage, "oui")==0){
+								decolle(pistes->liste, pistes);
+								clrscr();
+								animationAvion(1);
+							}
+						}
+						//Si avion en vol
+						if(avionAAfficher->etat == 1){
+							printf("Voulez vous faire atterir l'avion ? (oui ou non)\n");
+							scanf("%s", atterissage);
+							if(strcmp(atterissage, "oui")==0){
+								affichePisteAvion(pistes);
+								printf("\n");
+								printf("Sur quelle piste voulez-vous faire atterir l'avion ? (donner son numéro) ");
+								scanf("%d", &pisteAtterissage);
+								piste* pisteA = getPisteWithName(pisteAtterissage, pistes);
+								atterir(avionAAfficher, pisteA);
+								clrscr();
+								printf("ATTERISSAGE\n");
+								//animationAvion(2);
+							}
+						}
 					}
+					afficheAvion(avionAAfficher);
 					printf("\n");
 					printf("Appuyer sur 1 pour quitter et retourner au menu principal");
 					printf("\n");
@@ -67,15 +99,15 @@ void menu(listeAvion* liste, piste* PisteTEST){
 				//Afficher pistes
 				clrscr();
 				printf("---------------------------------------------------------- Liste des pistes ----------------------------------------------------------\n");
-				affichePisteAvion(PisteTEST);
+				affichePisteAvion(pistes);
 				printf("--------------------------------------------------------------------------------------------------------------------------------------\n");
 				printf("Quelle piste voulez-vous afficher ? ");
 				scanf("%d", &choixPistes);
 				printf("\n");
 				printf("\n");
 				while(quitter != 1){
-					piste* pisteAAfficher = getPisteWithName(choixPistes, PisteTEST);
-					affichePisteAvion(pisteAAfficher);
+					piste* pisteAAfficher = getPisteWithName(choixPistes, pistes);
+					affichePiste(pisteAAfficher);
 					printf("\n");
 					printf("Appuyer sur 1 pour quitter et retourner au menu principal");
 					printf("\n");
